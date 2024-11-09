31a9c45a2efa11535eca980c35b988af67bb7522
nanda0311 <nk3112002@gmail.com>
2024-11-09 12:50:41 +0530

    working

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,10 @@ import Navbar from './components/navbar';
 import Home from './pages/Homepage';
 import About from './pages/about';
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Menu from './layouts/Menu';
 import Header from './layouts/Header';
 import Footer from './layouts/Footer';
-import Home from './pages/Homepage';
+
 
 
 function App() {
