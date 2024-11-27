2796bed685144964ef34aa0a3a2ab8c6b62803d8
Edwinrajptu <edwininfant1227@gmail.com>
2024-11-28 00:25:05 +0530

    services

--- src/App.jsx
+++ src/App.jsx
@@ -19,16 +19,13 @@ import Header from './layouts/Header';
 import Footer from './layouts/Footer';
 import Contact from './pages/contact.jsx';
 import Partners from './pages/Partners.jsx';
-<<<<<<< HEAD
 import Service from './pages/Service.jsx';
 
-=======
 import Academic from './components/Partners/Academic.jsx';
 import Cooperative from './components/Partners/Co-operative.jsx';
 import Investment from './components/Partners/Investment.jsx';
 import IP from './components/Partners/IP.jsx';
 import Network from './components/Partners/Network.jsx';
->>>>>>> 63824dad8402706a31662ca2e0030c0f91c08d90
 
 
 function App() {
@@ -39,9 +36,9 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/partners" element={<Partners />} />
-<<<<<<< HEAD
+
         <Route path="/studentProject" element={<Service/>} />
-=======
+
         <Route path="/startupDetail" element={<Portfolio />} />
         <Route path="*" element={<div>404 - Page Not Found</div>} />
         <Route path="/academicPartners" element={<Academic />} />
@@ -50,7 +47,7 @@ function App() {
         <Route path="/ipSupporters" element={<IP />} />
         <Route path="/networkingPartners" element={<Network />} />
 
->>>>>>> 63824dad8402706a31662ca2e0030c0f91c08d90
+ 
       </Routes>
       <Footer />
     </Router>
