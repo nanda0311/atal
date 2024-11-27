acede19bb35e1e15b9e4b4a392ee74d0bc0641dc
Fearlessdhanush <dhanushdevaki1@gmail.com>
2024-11-27 23:37:39 +0530

    portfolio

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,26 @@
-// src/App.js
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-
-import Home from './pages/Homepage';
 import React from 'react';
-import Header from './layouts/Header';
-import Footer from './layouts/Footer';
+
+// Pages
+import Home from './pages/Homepage';
 import Contact from './pages/contact.jsx';
 import Partners from './pages/Partners.jsx';
+import Portfolio from './pages/Portfolio.jsx';
 
-
+// Layouts
+import Header from './layouts/Header';
+import Footer from './layouts/Footer';
 
 function App() {
   return (
     <Router>
       <Header />
-      {/* <Navbar />  */}
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/home" element={<Home />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/partners" element={<Partners />} />
+        <Route path="/startupDetail" element={<Portfolio />} />
+        <Route path="*" element={<div>404 - Page Not Found</div>} />
       </Routes>
       <Footer />
     </Router>
