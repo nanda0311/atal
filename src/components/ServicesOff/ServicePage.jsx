b07ff8fcd319d6d50a0dfb75045447c07cfb45ea
Edwinrajptu <edwininfant1227@gmail.com>
2025-01-09 15:35:40 +0530

    service page mobile version

--- src/components/ServicesOff/ServicePage.jsx
+++ src/components/ServicesOff/ServicePage.jsx
@@ -12,56 +12,48 @@ const services = [
     logo: Picture1,
     details: `Model Name: SMTMATE 660 No of Feeder: 64 Element for mounting: 0201, 0402, 0603, 0805, 1206, SOT, SOD, SOP, SSOP, QFN, VCQFN, BGA. Max chip size: 40*40mm`,
     type: 'Prototype',
-
   },
   {
     title: '3D SCANNER',
     logo: Picture2,
     details: `Model name: EinScan Pro HD Scan Accuracy: upto 0.045mm Output Formats: OBJ, STL, ASC, PLY, P3, 3MF Supported OS: win10,(64bit)`,
     type: 'Prototype',
-
   },
   {
     title: '3D PRINTER',
     logo: Picture3,
     details: `Model Name: Raise3D E2 3D Printer Material Type: PLA Print Head Travel Speed: 15–150 mm/s Machine Code Type: GCODE`,
     type: 'Prototype',
-
   },
   {
     title: 'RESIN 3D PRINTER',
-      logo: Picture4,
-      details: `Model name: Phrozen Sonic Mega 8K 3D Printer size: 475x400x680mm Print volume: 330x185x400mm Max printing speed: 70mm/hr`,
-      type: 'Prototype',
-
+    logo: Picture4,
+    details: `Model name: Phrozen Sonic Mega 8K 3D Printer size: 475x400x680mm Print volume: 330x185x400mm Max printing speed: 70mm/hr`,
+    type: 'Prototype',
   },
   {
     title: 'WEGSTR PCB PROTOTYPING MACHINE',
-      logo: Picture5,
-      details: `Model Name: Wegstr CNC Milling workspace: (140*200*40) (X*Y*Z) mm Working Material: Wood,Aluminium,PCB,Copper,Gold,Silver.`,
-      type: 'Prototype',
-
+    logo: Picture5,
+    details: `Model Name: Wegstr CNC Milling  Working Material: Wood,Aluminium,PCB,Copper,Gold,Silver.`,
+    type: 'Prototype',
   },
   {
     title: 'LASER CUTTING MACHINE',
     logo: Picture6,
     details: `Model Name: Flux Beam Box Pro Operating Power: 50w Max Cutting Depth: 12mm Versatile: Cutting and Engraving`,
     type: 'Prototype',
-
   },
   {
     title: 'SKYRC 1080 CHARGER FOR LIPO',
     logo: Picture7,
     details: `Model Name: Tattu 22000mAh (6S) Dimension: 206mm Length x 91mm Width x 61mm Height Material: Lithium Polymer`,
     type: 'Prototype',
-
   },
   {
     title: 'CELL IMPEDANCE TESTER',
     logo: Picture8,
     details: `Model Name: BT3563A Max voltage measurement: 300v Response time: 10ms Resistance measurement ranges: 3 mΩ/30 mΩ/300 mΩ/3 Ω/ 30 Ω/300 Ω/3 kΩ`,
     type: 'Prototype',
-
   },
   {
     title: 'INVERTER WELDING MACHINE',
@@ -87,27 +79,23 @@ const services = [
     details: `Model name: Tarot FLIR Metal 3 Axis gimbal Milling workspace: (140*200*40) (X*Y*Z) mm Working Material: FLIR thermal imaging cameras Flir VUE 336, VUE640`,
     type: 'Prototype',
   },
-
   {
     title: 'LIDAR 360 SOFTWARE',
     logo: Picture13,
     details: `Software Name: LiDAR360 Installation: 2 systems`,
     type: 'Software',
-
   },
   {
     title: 'E survey software',
     logo: Picture14,
     details: `Version Name: E survey software Installation: 2 systems Usage: Earthwork volume calculations`,
     type: 'Software',
-
   },
   {
     title: 'Agisoft Software',
     logo: Picture15,
     details: `Version Name: Agisoft Cloud Installation: 2 systems Usage: site inspection, annotation and documentation integrated with cloud processing service`,
     type: 'Software',
-
   },
   {
     title: 'KEIL SOFTWARE',
@@ -127,28 +115,23 @@ const services = [
     details: `Model Name: ALTAIR MULTIPHYSICS/ MECHATRONICS ENGINEER SUITE No of users: 1(full package) + 30(lab license) Nature of License: Perpetual`,
     type: 'Software',
   },
-
-
   {
     title: '3DPRINTER',
     logo: Pic1,
     details: ``,
     type: 'Student innvation',
-
   },
   {
     title: 'CNC PLOTTER',
     logo: Pic2,
     details: ``,
     type: 'Student innvation',
-
   },
   {
     title: '3DSCANNER',
     logo: Pic3,
     details: ``,
     type: 'Student innvation',
-
   },
   {
     title: 'E-CUBE',
@@ -162,13 +145,12 @@ const services = [
     details: ``,
     type: 'Student innvation',
   },
-
 ];
 
 export default function Service() {
   const [filter, setFilter] = useState('All');
   const [expandedSection, setExpandedSection] = useState(null);
-  
+
   const handleFilter = (filterType) => {
     setFilter(filterType);
   };
@@ -182,7 +164,7 @@ export default function Service() {
   };
 
   return (
-    <div className="max-w-7xl mx-auto px-4 py-8">
+    <div className="max-w-7xl mx-auto px-4 py-8 ">
       <h1 className="text-3xl font-bold text-center mb-8">AIC-PECF Service & Supports</h1>
 
       {/* Desktop View */}
@@ -203,11 +185,11 @@ export default function Service() {
           ))}
         </div>
 
-        <div className="grid grid-cols-1 lg:grid-cols-3 md:grid-cols-2 gap-8">
+        <div className="grid grid-cols-1 lg:grid-cols-3 md:grid-cols-2 gap-20 ">
           {filteredServices.map((service) => (
             <div
               key={service.title}
-              className="bg-white p-6 rounded-lg shadow-md hover:shadow-lg transition-all duration-300 hover:scale-105 h-[400px] flex flex-col"
+              className="bg-white p-6 rounded-lg shadow-md hover:shadow-[rgba(0,0,0,0.35)_0px_5px_15px] transition-all duration-300 hover:scale-105 h-[350px] flex flex-col "
             >
               <img
                 src={service.logo}
